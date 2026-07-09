61c3e668f89595571b57fedba4d8e73efa1df674
AKERZII <juan.pablo.garibay.rmz@gmail.com>
2026-07-09 01:23:17 -0600

    fix: configurar hmr condicional para produccion y local

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        
-        hmr: {
-            protocol: 'wss',
+export default defineConfig(({ command, mode }) => {
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            // Si está en producción (Railway), usa 'wss'. Si es local, usa el estándar.
+            hmr: mode === 'production' ? { protocol: 'wss' } : true,
+            watch: {
+                ignored: ['**/storage/framework/views/**'],
+            },
         },
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+    };
 });
